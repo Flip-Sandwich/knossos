f4a5ef590c81445a64799939bf113b2d8a30c6de
ngld <ngld@tproxy.de>
2017-12-07 06:42:32 +0100

    Moved Travis build script to the proper section and fixed dep install for AppVeyor

--- releng/ci/appveyor_install.bat
+++ releng/ci/appveyor_install.bat
@@ -1,7 +1,7 @@
 set "PATH=C:\Python36\Scripts;%PATH%"
 
 pip install pipenv
-pipenv install
+pipenv install --skip-lock
 pipenv install etaprogress pypiwin32 comtypes
 
 pipenv run python tools/common/download_archive.py releng/windows/support/support.json
