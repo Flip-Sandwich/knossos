cb4fbbce4e80742e422495651ecf10efae10a847
ngld <ngld@tproxy.de>
2017-12-07 07:29:55 +0100

    Rewrote CI scripts

--- releng/ci/appveyor_install.bat
+++ releng/ci/appveyor_install.bat
@@ -1,8 +1,6 @@
 set "PATH=C:\Python36\Scripts;%PATH%"
 
-pip install pipenv
-pipenv install --skip-lock
-pipenv install etaprogress pypiwin32 comtypes
+pip install PyQt5 six requests requests_toolbelt ply raven semantic_version pypiwin32 comtypes pyinstaller etaprogress token_bucket
 
 pipenv run python tools/common/download_archive.py releng/windows/support/support.json
 npm install
