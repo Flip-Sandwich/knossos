cc40f41a8a833b49023e5513279b26b1ca2af7f5
ngld <ngld@tproxy.de>
2017-12-07 06:25:12 +0100

    Fix dependencies for AppVeyor and solve a missing directory issue on Ubuntu

--- releng/ci/appveyor_install.bat
+++ releng/ci/appveyor_install.bat
@@ -2,7 +2,7 @@ set "PATH=C:\Python36\Scripts;%PATH%"
 
 pip install pipenv
 pipenv install
-pipenv install etaprogress
+pipenv install etaprogress pypiwin32 comtypes
 
 pipenv run python tools/common/download_archive.py releng/windows/support/support.json
 npm install
