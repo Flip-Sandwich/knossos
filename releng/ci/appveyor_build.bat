242b450f18cb4ccc13873ee3c417a9b4f55d3ecf
ngld <ngld@tproxy.de>
2017-12-07 07:44:32 +0100

    Another CI fix

--- releng/ci/appveyor_build.bat
+++ releng/ci/appveyor_build.bat
@@ -2,7 +2,7 @@ set "PATH=%CD%\releng\windows\support;C:\Python36\Scripts;%PATH%"
 
 mkdir releng\windows\dist
 
-pipenv run python configure.py
+python configure.py
 if errorlevel 1 exit /b 1
 
 ninja installer
--- releng/ci/appveyor_install.bat
+++ releng/ci/appveyor_install.bat
@@ -2,5 +2,5 @@ set "PATH=C:\Python36\Scripts;%PATH%"
 
 pip install PyQt5 six requests requests_toolbelt ply raven semantic_version pypiwin32 comtypes pyinstaller etaprogress token_bucket
 
-pipenv run python tools/common/download_archive.py releng/windows/support/support.json
+python tools/common/download_archive.py releng/windows/support/support.json
 npm install
